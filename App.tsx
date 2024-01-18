9c18fbf0f2bcc0a508cee1d2a5fbbe692115cfe2
edutdutra <edutdutra@gmail.com>
2024-01-18 08:11:24 -0300

    add sign in screen skeleton

--- App.tsx
+++ App.tsx
@@ -1,7 +1,29 @@
+import {StatusBar} from "react-native";
+import {ThemeProvider} from "styled-components/native";
+import {Roboto_400Regular, Roboto_700Bold, useFonts} from "@expo-google-fonts/roboto";
+
 import {SignIn} from "./src/screens/SignIn";
+import {Loading} from "./src/components/Loading";
+
+import theme from "./src/theme";
 
 export default function App() {
-  return (
-      <SignIn />
-  );
+    const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+    if (!fontsLoaded) {
+        return (
+            <Loading/>
+        )
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <SignIn/>
+        </ThemeProvider>
+    );
 }
