b775f2ff0b11d8655d54d15b36f654f67f3749ba
edutdutra <edutdutra@gmail.com>
2024-01-29 09:19:49 -0300

    add departure screen

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,12 +1,52 @@
-import {Container, Content, Title} from "./styles";
-import {HomeHeader} from "../../components/HomeHeader";
-import {CarStatus} from "../../components/CarStatus";
+import {useRef} from "react";
+import {KeyboardAvoidingView, Platform, ScrollView, TextInput} from "react-native";
+
+import {Header} from "../../components/Header";
+import {Button} from "../../components/Button";
+import {TextAreaInput} from "../../components/TextAreaInput";
+import {LicensePlateInput} from "../../components/LicensePlateInput";
+
+import {Container, Content} from "./styles";
+
+const KeyboardAvoidingViewBehavior = Platform.OS === 'android' ? 'height' : 'position'
 
 export function Departure() {
+    const descriptionRef = useRef<TextInput>(null);
+
+    function handleDepartureRegister() {
+        console.log('OK')
+    }
 
     return (
         <Container>
+            <Header title="Saída"/>
 
+            <KeyboardAvoidingView behavior={KeyboardAvoidingViewBehavior}>
+                <ScrollView>
+                    <Content>
+                        <LicensePlateInput
+                            label="Placa do veículo"
+                            placeholder="BRA1234"
+                            onSubmitEditing={() => descriptionRef.current?.focus()}
+                            returnKeyType="next"
+                        />
+
+                        <TextAreaInput
+                            ref={descriptionRef}
+                            label="Finalidade"
+                            placeholder="Vou utilizar o veículo para..."
+                            onSubmitEditing={handleDepartureRegister}
+                            returnKeyType="send"
+                            blurOnSubmit
+                        />
+
+                        <Button
+                            title="Registrar saída"
+                            onPress={handleDepartureRegister}
+                        />
+                    </Content>
+                </ScrollView>
+            </KeyboardAvoidingView>
         </Container>
     );
 }
--- src/screens/Departure/styles.ts
+++ src/screens/Departure/styles.ts
@@ -7,14 +7,7 @@ export const Container = styled.View`
 
 export const Content = styled.View`
   flex: 1;
+  gap: 16px;
   padding: 32px;
-`
-
-export const Title = styled.Text`
-  color: ${({theme}) => theme.COLORS.BRAND_LIGHT};
-
-  font-size: ${({theme}) => theme.FONT_SIZE.XXXL}px;
-  font-family: ${({theme}) => theme.FONT_FAMILY.BOLD};
-  
-  text-align: center;
+  margin-top: 16px;
 `
