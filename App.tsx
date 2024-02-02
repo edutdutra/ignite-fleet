da6175d1b04430b6301a8fc68bcdec84d04d7d5b
edutdutra <edutdutra@gmail.com>
2024-02-02 08:27:18 -0300

    add offline infos to user

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import './src/libs/dayjs'
 import {StatusBar} from "react-native";
 import {AppProvider, UserProvider} from "@realm/react";
 import {ThemeProvider} from "styled-components/native";
+import {useNetInfo} from "@react-native-community/netinfo";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import {Roboto_400Regular, Roboto_700Bold, useFonts} from "@expo-google-fonts/roboto";
 
@@ -13,12 +14,15 @@ import {RealmProvider, syncConfig} from './src/libs/realm'
 import {Routes} from "./src/routes";
 import {SignIn} from "./src/screens/SignIn";
 import {Loading} from "./src/components/Loading";
+import {TopMessage} from "./src/components/TopMessage";
 
 
 import theme from "./src/theme";
+import {WifiSlash} from "phosphor-react-native";
 
 export default function App() {
     const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+    const netInfo = useNetInfo();
 
     if (!fontsLoaded) {
         return (
@@ -30,6 +34,10 @@ export default function App() {
         <AppProvider id={REALM_APP_ID}>
             <ThemeProvider theme={theme}>
                 <SafeAreaProvider style={{flex: 1, backgroundColor: theme.COLORS.GRAY_800}}>
+                    {
+                        !netInfo.isConnected && <TopMessage title="Você está off-line." icon={WifiSlash} />
+                    }
+
                     <StatusBar
                         barStyle="light-content"
                         backgroundColor="transparent"
